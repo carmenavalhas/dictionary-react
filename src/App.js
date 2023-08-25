a0f68154fbad6a09739be9a5a86024368aa535fe
Carmen <38879535+carmenavalhas@users.noreply.github.com>
2023-08-25 11:36:38 +0100

    Added API dictionary integration

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import logo from "./logo.png"
 import './App.css';
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
         <img src={logo} className="App-logo img-fluid"
         alt="logo" />
       </header>
-      Hi, there!
+      <main>
+        <Dictionary />
+      </main>
+      <footer className=" App-footer text-center">
+        2023, React - {" "} 
+        <a href="https://github.com/carmenavalhas/dictionary-react">open source</a> ⎪ coded by {" "}
+        <a href="https://carmenavalhas-portfolio.netlify.app/">Carmen Navalhas</a>
+      </footer>
+    </div>
     </div>
   );
 }
